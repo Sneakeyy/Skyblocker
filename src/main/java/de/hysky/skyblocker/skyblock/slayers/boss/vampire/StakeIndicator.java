4b0fd83fb33b7011f2190bb216424d509d2fe30d
Sneakeyy <lars.schumacher@mailbox.org>
2025-08-22 11:41:53 +0200

    Improved stake indicator, movement keys exit chests in dungeons

--- src/main/java/de/hysky/skyblocker/skyblock/slayers/boss/vampire/StakeIndicator.java
+++ src/main/java/de/hysky/skyblocker/skyblock/slayers/boss/vampire/StakeIndicator.java
@@ -5,15 +5,16 @@ import de.hysky.skyblocker.skyblock.slayers.SlayerManager;
 import de.hysky.skyblocker.skyblock.slayers.SlayerType;
 import de.hysky.skyblocker.utils.render.title.Title;
 import de.hysky.skyblocker.utils.render.title.TitleContainer;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Formatting;
 
-
 public class StakeIndicator {
     private static final Title title = new Title("skyblocker.rift.stakeNow", Formatting.RED);
-	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	private static final SoundEvent sound = SoundEvents.ITEM_TOTEM_USE;
+	private static final float indicatorVolume = 100f;
+	private static final float indicatorPitch = 0.1f;
 
 	public static void updateStake() {
 		if (!SkyblockerConfigManager.get().slayers.vampireSlayer.enableSteakStakeIndicator || !SlayerManager.isInSlayerType(SlayerType.VAMPIRE)) {
@@ -21,11 +22,8 @@ public class StakeIndicator {
             return;
         }
 		Entity slayerEntity = SlayerManager.getSlayerBossArmorStand();
-        if (slayerEntity != null && slayerEntity.getDisplayName().toString().contains("҉")) {
-            TitleContainer.addTitle(title);
-			if (CLIENT.player != null) {
-				CLIENT.player.playSound(SoundEvents.ITEM_TOTEM_USE, 100f, 1f);
-			}
+        if (slayerEntity != null && slayerEntity.getDisplayName().toString().contains("҉") && !SlayerManager.getBossFight().slain) {
+            TitleContainer.addTitleAndPlayCustomSound(title, sound, indicatorVolume, indicatorPitch);
         } else {
             TitleContainer.removeTitle(title);
         }
